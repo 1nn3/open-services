97bf7903e0db39fe0c9ab791a8cfe35d8d2d3d0c
Inne Huk <inne@sdf-eu.org>
2025-10-01 00:00:00 +0200

    Initial commit

---
+++ examples/get-ftp-anons.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+import libxml2
+for i in range(1, len(sys.argv)):
+	try:
+		doc = libxml2.parseFile(sys.argv[i])
+		for node in doc.xpathEval('//script[@id="ftp-anon"][contains(@output, "Anonymous FTP login allowed")]'):
+			address = node.xpathEval('../../../address/@addr')[0].content
+			port = node.xpathEval('../@portid')[0].content
+			hostname = node.xpathEval('../../../hostnames/hostname[1]/@name')[0].content
+			print(address, "\t", port, "\t", hostname)
+		doc.freeDoc()
+	except:
+		pass
